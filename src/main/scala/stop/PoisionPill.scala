85a7f75da53aac6a6c0ab91765f1463aa03f7822
yubrajpokharel <yubrajpokharel>
2017-12-13 01:16:55 -0600

    poisionPill example

--- src/main/scala/stop/PoisionPill.scala
+++ src/main/scala/stop/PoisionPill.scala
@@ -1,8 +1,33 @@
 package stop
 
+import akka.actor.{Actor, ActorSystem, PoisonPill, Props}
+
 /**
   * Created by yubrajpokharel on 12/13/17.
   */
-class PoisionPill {
 
+case object Message2
+
+class PoisionPill extends Actor {
+  override def receive = {
+    case Message2 => println("get an call from director")
+    case _ => println("Opps wrong number")
+  }
+
+  override def postStop() = {
+    println("actor is stopped by poision pill")
+  }
+
+}
+
+object runner
+  //extends App
+{
+  val system  = ActorSystem("poisionPill")
+  val actor = system.actorOf(Props[PoisionPill], name = "poisionPilledActor")
+  actor ! "hi how are you"
+  actor ! Message2
+  actor ! PoisonPill
+  system.stop(actor)
+  system.terminate()
 }
